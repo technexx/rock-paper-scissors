f53191b1c3dde73e65aded1efcf4ce1a2439a3ed
Brian Salomone <brian.j.salomone@gmail.com>
2023-05-29 11:51:50 -0700

    add basic game functions

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function getFistRoll() {
+function computerRoll() {
     let roll = getRandomNumber(0, 2)
 
-    console.log(roll)
-    if (roll == 0) return "ROCK"
-    if (roll == 1) return "PAPER"
-    if (roll == 2) return "SCISSORS"
+    if (roll == 0) return "rock"
+    if (roll == 1) return "paper"
+    if (roll == 2) return "scissors"
 }
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-console.log(getFistRoll())
+function gameRound(playerSelection, computerSelection) {
+    console.log("player selection is " + playerSelection)
+    console.log("comp selection is " + computerSelection)
+
+    if (playerSelection === computerSelection) {
+        return "Tie"
+    }
+
+    if (playerSelection === "rock") {
+        if (computerSelection === ("scissors")) return "Player Win"; return "Player Loss"
+    }
+
+    if (playerSelection === "paper") {
+        if (computerSelection ===("rock")) return "Player Win"; return "Player Loss"
+    }
+
+    if (playerSelection === "scissors") {
+        if (computerSelection === "paper") return "Player Win"; return "Player Loss"
+    }
+}
+
+console.log(gameRound("rock", computerRoll()))
